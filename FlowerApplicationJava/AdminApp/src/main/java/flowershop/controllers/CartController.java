c0a59030a77502a91efc7a17c60f71a3be82b015
Ngần Thường <nganthainhi@gmail.com>
2026-03-11 00:23:09 +0700

    user

---
+++ FlowerApplicationJava/AdminApp/src/main/java/flowershop/controllers/CartController.java
@@ -0,0 +1,47 @@
+package flowershop.controllers;
+
+import flowershop.services.SceneManager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
+public class CartController {
+
+    @FXML
+    public void goHome() {
+        SceneManager.switchScene("/fxml/UserHome.fxml", "User Home");
+    }
+
+    @FXML
+    public void goFlowers() {
+        SceneManager.switchScene("/fxml/Flowers.fxml", "Flowers");
+    }
+
+    @FXML
+    public void goCategories() {
+        SceneManager.switchScene("/fxml/Categories.fxml", "Categories");
+    }
+
+    @FXML
+    public void goContact() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setTitle("Contact");
+        alert.setContentText("Chưa code màn Contact.");
+        alert.showAndWait();
+    }
+
+    @FXML
+    public void goCart() {
+        SceneManager.switchScene("/fxml/Cart.fxml", "Cart");
+    }
+
+    @FXML
+    public void goCheckout() {
+        SceneManager.switchScene("/fxml/Checkout.fxml", "Checkout");
+    }
+
+    @FXML
+    public void handleLogout() {
+        AuthController.logout();
+    }
+}
---
+++ FlowerApplicationJava/AdminApp/src/main/java/flowershop/services/CartService.java
@@ -0,0 +1,4 @@
+package flowershop.services;
+
+public class CartService {
+}
